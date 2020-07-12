b31c1b3b490effef28d2976c0047011454932bde
fanhl <dyguests@qq.com>
2020-07-12 12:01:56 +0800

    refactor code

--- app/src/main/java/com/fanhl/aidldemo/client/MainActivity.kt
+++ app/src/main/java/com/fanhl/aidldemo/client/MainActivity.kt
@@ -6,33 +6,25 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.*
 import android.os.IBinder.DeathRecipient
-import android.text.TextUtils
-import android.view.View
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import com.fanhl.aidldemo.IMsgManager
 import com.fanhl.aidldemo.IReceiveMsgListener
 import com.fanhl.aidldemo.Msg
 import com.fanhl.aidldemo.client.MyService.MyBinder
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    var binder: MyBinder? = null
-    var mConnection: ServiceConnection? = null
-    private var mListView: RecyclerView? = null
-    private var mEditText: EditText? = null
+    private var binder: MyBinder? = null
+    private lateinit var mConnection: ServiceConnection
     private val mMsgs: MutableList<Msg> = ArrayList()
-    private var mAdapter: MainAdapter? = null
+    private lateinit var mAdapter: MainAdapter
     private var mIMsgManager: IMsgManager? = null
 
-    private val mHandler: Handler = object : Handler() {
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-            mAdapter!!.setNewData(mMsgs)
-            mAdapter!!.notifyDataSetChanged()
-        }
+    private val mHandler: Handler = Handler(Looper.getMainLooper()) {
+        mAdapter.setNewData(mMsgs)
+        mAdapter.notifyDataSetChanged()
+        true
     }
 
     private val mReceiveMsgListener: IReceiveMsgListener = object : IReceiveMsgListener.Stub() {
@@ -58,18 +50,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mListView = findViewById<View>(R.id.recycler_view) as RecyclerView
-        mEditText = findViewById<View>(R.id.et_msg) as EditText
+
+        btn_send.setOnClickListener { binder?.sendMsg(Msg(et_msg.text.toString().trim { it <= ' ' })) }
+        btn_exit.setOnClickListener { finish() }
+
         mAdapter = MainAdapter()
-        mListView!!.adapter = mAdapter
+        recycler_view.adapter = mAdapter
+
         mConnection = object : ServiceConnection {
-            override fun onServiceConnected(
-                componentName: ComponentName,
-                iBinder: IBinder
-            ) {
+            override fun onServiceConnected(componentName: ComponentName, iBinder: IBinder) {
                 binder = iBinder as MyBinder
-                val msgManager = IMsgManager.Stub.asInterface(iBinder)
-                mIMsgManager = msgManager
+                mIMsgManager = IMsgManager.Stub.asInterface(iBinder)
                 try {
                     mIMsgManager!!.asBinder().linkToDeath(mDeathRecipient, 0)
                     mIMsgManager!!.registerReceiveListener(mReceiveMsgListener)
@@ -82,16 +73,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         //注意Activity和Service是同一进程才能使用Intent通信
-        val intent = Intent(this@MainActivity, MyService::class.java)
-        bindService(intent, mConnection as ServiceConnection, Context.BIND_AUTO_CREATE) //开启服务
-        findViewById<View>(R.id.btn_send).setOnClickListener(View.OnClickListener {
-            if (TextUtils.isEmpty(mEditText!!.text.toString())) {
-                Toast.makeText(this@MainActivity, "消息为空", Toast.LENGTH_SHORT).show()
-                return@OnClickListener
-            }
-            binder!!.sendMsg(Msg(mEditText!!.text.toString().trim { it <= ' ' }))
-        })
-        findViewById<View>(R.id.btn_exit).setOnClickListener { finish() }
+        bindService(Intent(this@MainActivity, MyService::class.java), mConnection, Context.BIND_AUTO_CREATE) //开启服务
     }
 
     override fun onDestroy() {
@@ -104,7 +86,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         //解除绑定服务
-        unbindService(mConnection!!)
+        unbindService(mConnection)
         super.onDestroy()
     }
 }
